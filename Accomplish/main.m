53d64c09775297ea207016f82d372306b33211c8
Shrijit Singh <shrijitsingh99@shrijits-imac.local>
2015-10-02 12:57:19 +0530

    Initial Commit

---
+++ Accomplish/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Accomplish
+//
+//  Created by Shrijit Singh on 02/10/15.
+//  Copyright © 2015 Shrijit Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
